351abf730d796fb676ed2e935c69321c595845d8
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-08-27 14:44:19 +0530

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int *ptr;
+    ptr=&a;
+    cout<<"value of a- \n"<<a;
+    cout<<"value of a- \n"<<*ptr;
+    cout<<"value of a- \n"<<*(&a);
+    cout<<"value of a- \n"<<*(*(&ptr));
+    cout<<"value of a- \n"<<&a;
+    cout<<"value of a- \n"<<ptr;
+    return 0;
+}
