69123f3bedcb10534ccacadfa241432739e24f3a
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-09-03 14:43:45 +0530

    Add files via upload

---
+++ Pointer_to_objects.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class demo{
+    public:
+        int a;
+    void print(){
+        cout<<a<<"\n";
+    }
+};
+    int main(){
+        demo obj, *p;
+        p=&obj;
+        int demo::*ptr;
+        obj.*ptr=100;
+        obj.print();
+        p->*ptr=200;
+        p->print();
+        return 0;
+    }
