86eeade03ceeabd771be6569efd713509d879e01
Kshitiz Rana <ranakshitizgm@gmail.com>
2024-09-20 10:28:22 +0530

    ENH: New Programs added

---
+++ new_delete.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+class complex
+{
+    double real, img ;
+public:
+    complex(double real, double img){
+        this->real =real;
+        this->img=img;
+    }
+    void getvalue(int a,int b){
+        real =a;
+        img = b;
+    }
+    void display(){
+        cout<<real<<"+"<<img<<endl;
+    }
+    void* operator new(size_t size){
+        void *ptr;
+        ptr = malloc(size);
+        cout<< "Operator New Overloaded\n";
+        cout<<"The size allocated is "<<size<<" Bytes"<<endl;
+        return ptr;
+    }
+
+    void operator delete(void* ptr){
+        cout<< "Delete op overloaded\n";
+        cout<<"Free Memory\n";
+        free(ptr);
+    }
+
+};
+int main(){
+    complex *p1;
+    p1 =new complex(5,6); //New op overloaded function
+    p1-> display();
+    delete(p1); // delete op function called
+    return 0;
+}
