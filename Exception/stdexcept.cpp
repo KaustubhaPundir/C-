56f1e46183ae8a7bee4833e9fead1596003523a2
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2024-11-13 15:08:49 +0530

    Create stdexcept.cpp

---
+++ Exception/stdexcept.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<stdexcept>
+using namespace std;
+int arraccess(int arr[],int index,int size){
+    if(index<0||index>=size){
+        throw out_of_range("index out of bound");
+    }
+    return arr[index];
+}
+
+int main()
+{
+   int numbers[]={10,20,30,40,50};
+   int size=sizeof(numbers)/sizeof(numbers[0]);
+   int index;
+   cout<<"Enter index: ";
+   cin>>index;
+   try{
+    int result=arraccess(numbers,index,size);
+    cout<<"value at index: "<<index<<result;
+   }
+   catch(exception &e){
+    cout<<"Exception:"<<e.what();
+   }
+}
